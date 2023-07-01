f2f145b2e4537b60cc65e86acb6a1ce7a4c4c39c
Xayden13 <seemsdicey@gmail.com>
2023-06-30 21:13:28 -0400

    feat: add responisve layout to player components

--- components/PlayerContainer/index.js
+++ components/PlayerContainer/index.js
@@ -2,48 +2,61 @@ import { useState } from 'react';
 import Button from '../Button';
 import styles from './PlayerContainer.module.css';
 import factions from '@/schemas/factions';
+import { Box, MenuItem, Select, TextField, InputLabel } from '@mui/material';
 
-const PlayerContainer = props => {
-    const { handleSubmit } = props;
-    const [playerDetails, setPlayerDetails] = useState({
-        faction: '',
-        name: ''
+const PlayerContainer = (props) => {
+  const { handleSubmit } = props;
+  const [playerDetails, setPlayerDetails] = useState({
+    faction: '',
+    name: '',
+  });
+
+  const handeDetailChange = (e) => {
+    const { name, value } = e.target;
+
+    setPlayerDetails({
+      ...playerDetails,
+      [name]: value,
     });
+  };
 
-    const handeDetailChange = e => {
-        const {name, value} = e.target;
-
-        setPlayerDetails({
-            ...playerDetails,
-            [name]: value
-        });
-    };
-    
-    return (
-        <div className={styles.PlayerContainer}>
-            <div className={styles.PlayerInputContainer}>
-                <label>Name: </label>
-                <input
-                    name={'name'}
-                    placeholder='Name'
-                    value={playerDetails.name}
-                    onChange={e => handeDetailChange(e)}
-                />
-            </div>
-            <div className={styles.PlayerInputContainer}>
-                <label>Faction: </label>
-                <select className={styles.FactionSelect} name="faction" onChange={e => handeDetailChange(e)} style={{width: '100%'}}>
-                    <option disabled selected>Select A Faction</option>
-                    {factions.map((faction, index) => <option key={index} data-id={faction}>{faction}</option>)}
-                </select>
-            </div>
-            <Button 
-                disabled={!playerDetails.faction || !playerDetails.name}
-                handleSubmit={() => handleSubmit(playerDetails)}
-                text='Continue'
-            />
-        </div>
-    )
+  return (
+    <Box className="primary-box text-center p-3 pt-5 ">
+      <TextField
+        className="mb-3 mr-3 md:mr-0"
+        variant="outlined"
+        name={'name'}
+        label="Name"
+        value={playerDetails.name}
+        onChange={(e) => handeDetailChange(e)}
+      />
+      <div className={styles.PlayerInputContainer}>
+        <InputLabel id="faction-select-label">Faction</InputLabel>
+        <Select
+          labelId="faction-select-label"
+          className={styles.FactionSelect}
+          name="faction"
+          label="Faction"
+          onChange={(e) => handeDetailChange(e)}
+          style={{ width: '100%' }}
+        >
+          <MenuItem disabled selected>
+            Select A Faction
+          </MenuItem>
+          {factions.map((faction, index) => (
+            <MenuItem key={index} id={faction} value={faction}>
+              {faction}
+            </MenuItem>
+          ))}
+        </Select>
+      </div>
+      <Button
+        disabled={!playerDetails.faction || !playerDetails.name}
+        handleSubmit={() => handleSubmit(playerDetails)}
+        text="Continue"
+      />
+    </Box>
+  );
 };
 
-export default PlayerContainer
+export default PlayerContainer;
--- components/PlayerStats/index.js
+++ components/PlayerStats/index.js
@@ -1,75 +1,127 @@
-import styles from './PlayerStats.module.css';
+import { Grid } from '@mui/material';
+import classNames from 'classnames';
 
-const PlayerStats = props => {
-    const {
-        attacker,
-        cp,
-        currentTurn,
-        faction,
-        location,
-        name,
-        player,
-        primaryPoints,
-        secondaries,
-        secondaryPoints
-    } = props;
+const PlayerStats = (props) => {
+  const {
+    attacker,
+    cp,
+    currentTurn,
+    faction,
+    location,
+    name,
+    player,
+    primaryPoints,
+    secondaries,
+    secondaryPoints,
+  } = props;
 
-    const isCurrentTurn = currentTurn === player;
-    const isAttacker = attacker === player;
-    const styleBlock = {
-        [location]: 0
-    };
+  const isCurrentTurn = currentTurn === player;
+  const isAttacker = attacker === player;
 
-    return (
-        <div>
-            <div className={`${styles.PlayerStatsContainer} ${isCurrentTurn ? `${styles.CurrentTurn}` : ''}`} style={{...styleBlock}}>
-                <p className={styles.PlayerTitle}>Player {location === 'left' ? 'One' : 'Two'}</p>
-                {attacker &&
-                    <p className={isAttacker ? styles.Attacker : styles.Defender}>{isAttacker ? 'Attacker' : 'Defender'}</p>        
-                }
-                <p>Name: {name}</p>
-                <p>Faction: {faction}</p>
-                <p>CP: {cp}</p>
-                <p>Primary Points: {primaryPoints}</p>
-                <p>Seconary Points: {secondaryPoints}</p>
-            </div>
-            {secondaries && secondaries.length > 0 && location === 'left' &&
-            <div className={`${styles.AllSecondaries} ${styles.LeftAllSecondaries}`}>
-                {secondaries && secondaries.map((secondary, index) => {
-                    return (
-                        <div key={index} className={styles.SecondariesContainer}>
-                            <p className={styles.TurnMarker}>Turn {index + 1}</p>
-                            <div className={styles.SecondaryContainer}>
-                            <div style={{color: secondary[0].achieved && secondary[0].achieved === index + 1 ? 'green' : 'black'}}>{secondary[0].name}</div>
-                            </div>
-                            <div className={styles.SecondaryContainer}>
-                            <div style={{color: secondary[1].achieved && secondary[0].achieved === index + 1 ? 'green' : 'black'}}>{secondary[1].name}</div>
-                            </div>
-                        </div>
-                    )
-                })}
-            </div>
+  const containerClasses = classNames(
+    'primary-box p-3',
+    {
+      'border-red-500': isCurrentTurn,
+    }
+  );
+
+  const secondarisBorder = classNames('border-solid border-1 border-black');
+
+  return (
+    <>
+      <div className={containerClasses}>
+        <p className="font-bold text-center underline">
+          Player {location === 'left' ? 'One' : 'Two'}
+        </p>
+        {attacker && (
+          <p
+            className={
+              isAttacker ? 'decoration-red-500' : 'decoration-green-500'
             }
-            {secondaries && secondaries.length > 0 && location === 'right' &&
-            <div className={`${styles.AllSecondaries} ${styles.RightAllSecondaries}`}>
-                {secondaries && secondaries.map((secondary, index) => {
-
-                    return (
-                        <div key={index} className={styles.SecondariesContainer}>
-                            <p className={styles.TurnMarker}>Turn {index + 1}</p>
-                            <div className={styles.SecondaryContainer}>
-                                <div style={{color: secondary[0].achieved && secondary[0].achieved === index + 1 ? 'green' : 'black'}}>{secondary[0].name}</div>
-                            </div>
-                            <div className={styles.SecondaryContainer}>
-                                <div style={{color: secondary[1].achieved ? 'green' : 'black'}}>{secondary[1].name}</div>
-                            </div>
-                        </div>
-                    )
-                })}
-            </div>
-            }
+          >
+            {isAttacker ? 'Attacker' : 'Defender'}
+          </p>
+        )}
+        <p>Name: {name}</p>
+        <p>Faction: {faction}</p>
+        <p>CP: {cp}</p>
+        <p>Primary Points: {primaryPoints}</p>
+        <p>Seconary Points: {secondaryPoints}</p>
+      </div>
+      {secondaries && secondaries.length > 0 && location === 'left' && (
+        <div className="ml-3 top-20">
+          {secondaries &&
+            secondaries.map((secondary, index) => {
+              return (
+                <div key={index} className={secondarisBorder}>
+                  <p className="underline">Turn {index + 1}</p>
+                  <div className={secondarisBorder}>
+                    <div
+                      style={{
+                        color:
+                          secondary[0].achieved &&
+                          secondary[0].achieved === index + 1
+                            ? 'green'
+                            : 'black',
+                      }}
+                    >
+                      {secondary[0].name}
+                    </div>
+                  </div>
+                  <div className={secondarisBorder}>
+                    <div
+                      style={{
+                        color:
+                          secondary[1].achieved &&
+                          secondary[0].achieved === index + 1
+                            ? 'green'
+                            : 'black',
+                      }}
+                    >
+                      {secondary[1].name}
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
         </div>
-    )
+      )}
+      {secondaries && secondaries.length > 0 && location === 'right' && (
+        <div className="mr-3 top-20">
+          {secondaries &&
+            secondaries.map((secondary, index) => {
+              return (
+                <div key={index} className={secondarisBorder}>
+                  <p className="underline">Turn {index + 1}</p>
+                  <div className={secondarisBorder}>
+                    <div
+                      style={{
+                        color:
+                          secondary[0].achieved &&
+                          secondary[0].achieved === index + 1
+                            ? 'green'
+                            : 'black',
+                      }}
+                    >
+                      {secondary[0].name}
+                    </div>
+                  </div>
+                  <div className={secondarisBorder}>
+                    <div
+                      style={{
+                        color: secondary[1].achieved ? 'green' : 'black',
+                      }}
+                    >
+                      {secondary[1].name}
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+        </div>
+      )}
+    </>
+  );
 };
 
-export default PlayerStats
+export default PlayerStats;
